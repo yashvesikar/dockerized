df29cae244c4bf2de8195d7cdc201f1dbf0ba3b7
Yash Vesikar <yashvesikar@gmail.com>
2020-11-19 17:59:25 -0800

    Updating the repo

--- packages/landingpage/pages/index.tsx
+++ packages/landingpage/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
 
-const IndexPage = () => <h1>Welcome to the Dockerized App!</h1>
+const IndexPage = () => <h1>Welcome to the Dockerized App! This is my personl website</h1>
 
 export default IndexPage
